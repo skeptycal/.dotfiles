97b2be7fcb21538516da4a5ccc42b8a574ee8d36
skeptycal <skeptycal@gmail.com>
2019-11-01 15:21:04 -0500

    initial commit

---
+++ skeptycal_mit_license.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# -*- coding: utf-8 -*-
+# cannot be run directly; called from .bash_profile or .bashrc
+BASH_SOURCE=${(%):-%N} # to ease the transition to zsh
+
+# data
+# curl -H 'Content-Type: application/json' -d''
