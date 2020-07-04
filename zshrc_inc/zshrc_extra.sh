383cfbc04f7a2a6746b40367671cdb2e874eb562
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-07-03 22:56:46 -0500

    # (GitIt) minor updates # https://www.github.com/skeptycal

--- zshrc_inc/zshrc_extra.sh
+++ zshrc_inc/zshrc_extra.sh
@@ -4,21 +4,20 @@
     # shellcheck source=/dev/null
     # shellcheck disable=SC2034
 
-# cannot be run directly; called from .bash_profile or .bashrc or .zshrc
-# BASH_SOURCE=${(%):-%N} # to ease the transition to zsh
-# *############################################################################
-# [[ $SET_DEBUG == 1 ]] && db_script_source ".extra test"
+#? ######################## Heroku
+    # Heroku autocomplete setup
+    # heroku autocomplete --refresh-cache # one time use after above is loaded
 
 
-# https://rick.cogley.info/post/use-homebrew-zsh-instead-of-the-osx-default/
-# unalias run-help
-# autoload run-help
-# HELPDIR=/usr/local/share/zsh/help
+    # https://rick.cogley.info/post/use-homebrew-zsh-instead-of-the-osx-default/
+    # unalias run-help
+    # autoload run-help
+    # HELPDIR=/usr/local/share/zsh/help
 
 
-# git config --global user.name "$GIT_AUTHOR_NAME"
-# git config --global user.email "$GIT_AUTHOR_EMAIL"
-# git config --global user.signingkey "$GIT_SIGNINGKEY"
+    # git config --global user.name "$GIT_AUTHOR_NAME"
+    # git config --global user.email "$GIT_AUTHOR_EMAIL"
+    # git config --global user.signingkey "$GIT_SIGNINGKEY"
 
 
-# source $(which brew_fix.sh)
+    # source $(which brew_fix.sh)
