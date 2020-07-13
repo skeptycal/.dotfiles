72cd6a2319ab0e49c009a628cc30d0c3b405a0ac
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-07-13 16:43:44 -0600

    Update move_comments.py

--- move_comments.py
+++ move_comments.py
@@ -5,49 +5,6 @@ from pathlib import Path
 # from dataclasses import dataclass, field, Field
 
 
-class PathPackError(IOError):
-    ''' Error processing PathPack path. '''
-
-
-class PathPack(Path):
-    ''' pathlib.Path subclass to encapsulate more common methods.
-    '''
-    path_name: (Path) = '.'
-    # _path: Field = field(init=False)
-
-    def __init__(self, path_name):
-        super().__init__()
-        self.path_name = path_name
-        self._path: Path = Path(self.path_name)
-
-    @property
-    def path(self):
-        if not self._path:
-            self._path = Path(self.path_name)
-        return self._path
-
-    @path.setter
-    def path(self, value):
-        if isinstance(value, str):
-            try:
-                self._path = Path(value).resolve()
-            except IOError:
-                raise PathPackError(
-                    f'Error converting {self.path_name} to Path.')
-        if not self._path.exists():
-            try:
-                self._path().touch()
-            except IOError:
-                raise PathPackError(f'Error creating {self.path_name}')
-
-    @property
-    def path_str(self):
-        return self.path.as_posix()
-
-    def __str__(self):
-        return self.path_str
-
-
 p = PathPack('.')
 
 
